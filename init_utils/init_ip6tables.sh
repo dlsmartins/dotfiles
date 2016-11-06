a4140d0471aae609a5b456ba3b6f44cb92292e5a
dlsmartins <gdanielmartins@gmail.com>
2016-11-06 18:55:51 +0100

    added iptables init scripts

---
+++ init_utils/init_ip6tables.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+ITCMD="ip6tables"
+IFPUB="eth0"
+IFLO="lo"
+
+# CLEAN EVERYTHING
+$ITCMD -F
+$ITCMD -X
+
+# CREATE NEW CUSTOM CHAINS
+$ITCMD -N TCP
+$ITCMD -N UDP
+
+# ALLOW ANYTHING ON THE LOCAL LINK
+$ITCMD -A INPUT -i $IFLO -j ACCEPT
+$ITCMD -A OUTPUT -o $IFLO -j ACCEPT
+
+# ALLOW EVERYTHING TO GO OUT ON THE INTERNET
+$ITCMD -A OUTPUT -o $IFPUB -j ACCEPT
+
+# ALLOW ESTABLISHED, RELATED PACKETS BACK IN
+$ITCMD -A INPUT -i $IFPUB -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# DROP INVALID PACKETS
+$ITCMD -A INPUT -m conntrack --ctstate INVALID -j DROP
+
+# ALLOW INCOMING ICMPV6 ERROR REPLY PACKETS TO COME IN
+$ITCMD -A INPUT -p icmpv6 --icmpv6-type destination-unreachable -j ACCEPT
+$ITCMD -A INPUT -p icmpv6 --icmpv6-type packet-too-big -j ACCEPT
+$ITCMD -A INPUT -p icmpv6 --icmpv6-type time-exceeded -j ACCEPT
+$ITCMD -A INPUT -p icmpv6 --icmpv6-type parameter-problem -j ACCEPT
+
+# ALLOW INCOMING ICMPV6 ECHO PACKETS TO COME IN (RATE LIMITED)
+$ITCMD -A INPUT -p icmpv6 --icmpv6-type echo-request -m limit --limit 900/min -j ACCEPT
+$ITCMD -A INPUT -p icmpv6 --icmpv6-type echo-reply -m limit --limit 900/min -j ACCEPT
+
+# ALLOW OTHER ICMPV6 PACKET TYPES (IF HOP-LIMIT IS 255)
+$ITCMD -A INPUT -p icmpv6 --icmpv6-type router-advertisement -m hl --hl-eq 255 -j ACCEPT
+$ITCMD -A INPUT -p icmpv6 --icmpv6-type neighbor-solicitation -m hl --hl-eq 255 -j ACCEPT
+$ITCMD -A INPUT -p icmpv6 --icmpv6-type neighbor-advertisement -m hl --hl-eq 255 -j ACCEPT
+$ITCMD -A INPUT -p icmpv6 --icmpv6-type redirect -m hl --hl-eq 255 -j ACCEPT
+
+# DROP PACKETS WITH RH0 HEADERS
+$ITCMD -A INPUT -m rt --rt-type 0 -j DROP
+$ITCMD -A FORWARD -m rt --rt-type 0 -j DROP
+$ITCMD -A OUTPUT -m rt --rt-type 0 -j DROP
+
+# ALLOW LINK-LOCAL ADDRESSES
+$ITCMD -A INPUT -s fe80::/10 -j ACCEPT
+$ITCMD -A OUTPUT -s fe80::/10 -j ACCEPT
+
+# ALLOW MULTICAST
+$ITCMD -A INPUT -d ff00::/8 -j ACCEPT
+$ITCMD -A OUTPUT -d ff00::/8 -j ACCEPT
+
+# PASS REMAINING PACKETS THROUGH CUSTOM CHAINS
+$ITCMD -A INPUT -p tcp -j TCP
+$ITCMD -A INPUT -p udp -j UDP
+
+# PREVENT SYN-FLOOD (DROP EVERY NEW PACKET WHICH IS NOT A SYN)
+$ITCMD -A TCP -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+
+# ALLOW INCOMING TCP ON 80 (HTTP)
+$ITCMD -A TCP -p tcp -m tcp --dport 80 -j ACCEPT
+
+# ALLOW INCOMING TCP ON 443 (HTTPS/SSH)
+$ITCMD -A TCP -p tcp -m tcp --dport 443 -j ACCEPT
+
+# REJECT EVERYTHING ELSE (TCP)
+$ITCMD -A TCP -p tcp -j REJECT --reject-with tcp-reset
+
+# REJECT EVERYTHING ELSE (UDP)
+$ITCMD -A UDP -p udp -j REJECT --reject-with icmp6-port-unreachable
+
+# SET DEFAULT POLICIES TO DROP
+$ITCMD -P INPUT DROP
+$ITCMD -P FORWARD DROP
+$ITCMD -P OUTPUT DROP
---
+++ init_utils/init_iptables.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+ITCMD="iptables"
+IFPUB="eth0"
+IFLO="lo"
+
+# CLEAN EVERYTHING
+$ITCMD -F
+$ITCMD -X
+
+# CREATE NEW CUSTOM CHAINS
+$ITCMD -N TCP
+$ITCMD -N UDP
+
+# ALLOW ANYTHING ON THE LOCAL LINK
+$ITCMD -A INPUT -i $IFLO -j ACCEPT
+$ITCMD -A OUTPUT -o $IFLO -j ACCEPT
+
+# ALLOW EVERYTHING TO GO OUT ON THE INTERNET
+$ITCMD -A OUTPUT -o $IFPUB -j ACCEPT
+
+# ALLOW ESTABLISHED, RELATED PACKETS BACK IN
+$ITCMD -A INPUT -i $IFPUB -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# DROP INVALID PACKETS
+$ITCMD -A INPUT -m conntrack --ctstate INVALID -j DROP
+
+# ALLOW INCOMING ICMP ERROR REPLY PACKETS TO COME IN
+$ITCMD -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT
+$ITCMD -A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
+$ITCMD -A INPUT -p icmp --icmp-type parameter-problem -j ACCEPT
+
+# ALLOW INCOMING ICMP ECHO PACKETS TO COME IN (RATE LIMITED)
+$ITCMD -A INPUT -p icmp --icmp-type echo-request -m limit --limit 900/min -j ACCEPT
+$ITCMD -A INPUT -p icmp --icmp-type echo-reply -m limit --limit 900/min -j ACCEPT
+
+# PASS REMAINING PACKETS THROUGH CUSTOM CHAINS
+$ITCMD -A INPUT -p tcp -j TCP
+$ITCMD -A INPUT -p udp -j UDP
+
+# PREVENT SYN-FLOOD (DROP EVERY NEW PACKET WHICH IS NOT A SYN)
+$ITCMD -A TCP -p tcp ! --syn -m conntrack --ctstate NEW -j REJECT --reject-with tcp-reset
+
+# ALLOW INCOMING TCP ON 80 (HTTP)
+$ITCMD -A TCP -p tcp -m tcp --dport 80 -j ACCEPT
+
+# ALLOW INCOMING TCP ON 443 (HTTPS/SSH)
+$ITCMD -A TCP -p tcp -m tcp --dport 443 -j ACCEPT
+
+# REJECT EVERYTHING ELSE (TCP)
+$ITCMD -A TCP -p tcp -j REJECT --reject-with tcp-reset
+
+# REJECT EVERYTHING ELSE (UDP)
+$ITCMD -A UDP -p udp -j REJECT --reject-with icmp-port-unreachable
+
+# SET DEFAULT POLICIES TO DROP
+$ITCMD -P INPUT DROP
+$ITCMD -P FORWARD DROP
+$ITCMD -P OUTPUT DROP
